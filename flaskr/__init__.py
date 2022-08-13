8cff20f01234c2412b48fe84b4fe48fbc1b00381
Carlo <carlosaprado@gmail.com>
2022-08-13 16:02:15 -0300

    blog views and static assets

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@
 
 import os
 from flask import Flask
-from . import db, auth
+from . import db, auth, blog
 
 def create_app(test_config=None):
     # create and configure the app
@@ -35,5 +35,8 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(auth.bp)
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
