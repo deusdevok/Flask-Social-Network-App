73cc09dbeae58d4cd6989e968535ab6aeb2b4701
Carlo <carlosaprado@gmail.com>
2022-09-05 17:24:08 -0300

    pagination (not finished right)

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -25,6 +25,27 @@ def index():
 
     return render_template('blog/index.html', posts=posts, all_tags = all_tags)
 
+@bp.route('/page/<page_index>')
+def index_by_page(page_index):
+    page_index = int(page_index)
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username, tags'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+        ' LIMIT ? OFFSET ?',
+        (5, 5*page_index-5)
+    ).fetchall()
+    
+
+    all_tags = []
+    for post in posts:
+        all_tags += post['tags'].split(',')
+
+    all_tags = list(set(all_tags))
+
+    return render_template('blog/index.html', posts=posts, all_tags = all_tags)
+
 @bp.route('/tag/<tag>')
 def viewtag(tag):
     db = get_db()
