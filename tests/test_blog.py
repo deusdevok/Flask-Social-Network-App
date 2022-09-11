f32f27115a4bda7c60c70d3ada615cecf58a457d
bionic <carlosaprado@gmail.com>
2022-09-11 13:14:41 -0300

    edit test

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -4,7 +4,7 @@ from flaskr.db import get_db
 
 def test_index(client, auth):
     response = client.get('/')
-    assert b"Log In" in response.data
+    assert b"Log In" in response.data # b prefix means "bytes string literal"
     assert b"Register" in response.data
 
     auth.login()
